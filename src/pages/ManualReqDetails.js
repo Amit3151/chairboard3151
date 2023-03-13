5ac385029904e5b86e41081811039111adfb72e7
Amit Kumar Yadav <amitgda280@gmail.com>
2023-03-13 17:36:13 +0530

    13-03-23 Amit Update

--- src/pages/ManualReqDetails.js
+++ src/pages/ManualReqDetails.js
@@ -6,19 +6,23 @@ import '../css/ManualReqDetails.css'
 import sort from "../images/uil_sort-amount-down.svg";
 import Header from "../components/Header";
 import Sidebar from '../components/Sidebar';
+import Slider from "react-slick";
 import Filter from '../components/Filter';
 import { Link } from "react-router-dom";
 import cross from "../images/cross-23.svg";
 import upload from "../images/ic_outline-cloud-upload.svg";
 import download from "../images/download.svg";
-import id_bluespace from '../images/id_bluespace.png'
+import id_bluespace from '../images/id_bluespace.png';
+
+
+
 
 export default function ManualReqDetails() {
 
 
     // Creating Dummy Static Data For Channnel Partner Table
     const tableData = [
-        { Id: '1', VehicleClass: 'Vc4', Quantity: '25', Bank: 'Kotak', Rate: '1', Amount: '25', UploadedInVentory: 0, },
+        { TagId: 'daiwd799sdadfa6adasd8', InputId: 'Vehicle number', VRN: 'RJ45 CM 2222', VC: '4', Mobile: '9987654321', },
     ]
 
     //Pop up upload
@@ -44,6 +48,19 @@ export default function ManualReqDetails() {
         alert("Accepted Successfully")
     }
 
+
+
+    const [slider_click, setSlider_click] = useState(false);
+    function open_popub_slider() {
+        setSlider_click(true)
+    }
+    function close_slider_popup() {
+        setSlider_click(false)
+    }
+    const [nav1, setNav1] = useState();
+    const [nav2, setNav2] = useState();
+    const popup_box_slider = slider_click ? "popup_box_slider_main opened" : "popup_box_slider_main";
+
     return (
         <>
             <div className="sidebar">
@@ -61,10 +78,12 @@ export default function ManualReqDetails() {
                                     <h3>Request Type : Issue</h3>
                                 </div>
                                 <div className="odbtns mrd">
+
+                                    <input className="enterAmount" placeholder="Enter Amount" />
                                     <Link to='/ManualRequest'>
-                                        <button id="mrdbtnone">Cancel Order</button>
+                                        <button id="mrdbtnone">Decline Request</button>
                                     </Link>
-                                    <button id='mrdbtntwo' onClick={Accept}>Accept</button>
+                                    <button id='mrdbtntwo' onClick={Accept}>Accept Request</button>
                                 </div>
 
                             </div>
@@ -79,47 +98,42 @@ export default function ManualReqDetails() {
                                             <tr>
                                                 <td>
                                                     <span className="align">
-                                                        Vehicle Class <img src={sort} alt="" />
+                                                        Tag ID <img src={sort} alt="" />
                                                     </span>
                                                 </td>
                                                 <td>
                                                     <span className="align">
-                                                        Quantity<img src={sort} alt="" />
+                                                        Input Type<img src={sort} alt="" />
                                                     </span>
                                                 </td>
                                                 <td>
                                                     <span className="align">
-                                                        Bank<img src={sort} alt="" />
+                                                        VRN<img src={sort} alt="" />
                                                     </span>
                                                 </td>
                                                 <td>
                                                     <span className="align">
-                                                        Rate<img src={sort} alt="" />
+                                                        VC<img src={sort} alt="" />
                                                     </span>
                                                 </td>
                                                 <td>
                                                     <span className="align">
-                                                        Amount<img src={sort} alt="" />
+                                                        Mobile<img src={sort} alt="" />
                                                     </span>
                                                 </td>
-                                                <td>
-                                                    <span className="align">
-                                                        Uploaded InVentory<img src={sort} alt="" />
-                                                    </span>
-                                                </td>
+
                                             </tr>
                                         </thead>
 
                                         <tbody>
                                             {tableData.map((item, index) => (
                                                 <tr key={index}>
-                                                    <td>{item.VehicleClass}</td>
-                                                    <td>{item.Quantity}</td>
-                                                    <td>{item.Bank}</td>
-                                                    <td>{item.Rate}</td>
-                                                    <td>{item.Amount}</td>
-                                                    <td>{item.UploadedInVentory}</td>
-                                                    <td>{item.DispatchOn}</td>
+                                                    <td>{item.TagId}</td>
+                                                    <td>{item.InputId}</td>
+                                                    <td>{item.VRN}</td>
+                                                    <td>{item.VC}</td>
+                                                    <td>{item.Mobile}</td>
+
                                                 </tr>
                                             ))}
                                         </tbody>
@@ -132,16 +146,92 @@ export default function ManualReqDetails() {
 
                             <div className='idHeading'>
                                 <h1 className='id_head'>Preview of Document</h1>
-                                <div className='idPhotos'>
-                                    <div><img className='idPhoto' src={id_bluespace} /> <br /> <h2>RC Front</h2></div>
-                                    <div><img className='idPhoto' src={id_bluespace} /> <br /> <h2>RC Back</h2></div>
+                                <div className='idimages'>
+                                    <div><img className='idPhoto' src={id_bluespace} onClick={open_popub_slider} /><h2 className='idimagesheading'>RC Front</h2></div>
+                                    <div><img className='idPhoto' src={id_bluespace} onClick={open_popub_slider} /><h2 className='idimagesheading'>RC Back</h2></div>
+                                </div>
+
+
+
+                                <div className={popup_box_slider}>
+                                    <div className='popup_box_slider'>
+                                        <div className='cross_icon_slider' onClick={close_slider_popup}>
+                                            <img src={cross} alt="" />
+                                        </div>
+                                        <div className='download_icon_slider'>
+                                            <a href={download} target="_blank" download>
+                                                <img src={download} alt="" />
+                                            </a>
+
+                                        </div>
+
+                                        <div className='main_display_popup_slider'>
+                                            <Slider
+                                                dots={false}
+                                                arrows={true}
+                                                infinite={true}
+                                                asNavFor={nav2} ref={(slider1) => setNav1(slider1)
+
+                                                }>
+                                                <div className='document_preview_slide_wrap'>
+                                                    <div className='image_name'>Rc Front</div>
+                                                    <div className='document_preview_slide'>
+                                                        <img src={id_bluespace} />
+                                                    </div>
+                                                </div>
+                                                <div className='document_preview_slide_wrap'>
+                                                    <div className='image_name'>Rc Front</div>
+                                                    <div className='document_preview_slide'>
+                                                        <img src={id_bluespace} />
+                                                    </div>
+                                                </div>
+                                                <div className='document_preview_slide_wrap'>
+                                                    <div className='image_name'>Rc Front</div>
+                                                    <div className='document_preview_slide'>
+                                                        <img src={id_bluespace} />
+                                                    </div>
+                                                </div>
+                                                
+                                            </Slider>
+                                        </div>
+                                        <div className='navigation_popup_slider manualreq'>
+                                            <Slider asNavFor={nav1}
+                                                ref={(slider2) => setNav2(slider2)}
+                                                slidesToShow={2}
+                                                swipeToSlide={true}
+                                                focusOnSelect={true}
+                                                dots={true}
+                                                arrows={true}
+                                                infinite={true}
+                                                speed={500}
+                                                slidesToScroll={1}
+                                            >
+                                                <div className='document_preview_slide_wrap'>
+                                                    <div className='document_preview_slide'>
+                                                        <img src={id_bluespace} />
+                                                    </div>
+                                                </div>
+                                                <div className='document_preview_slide_wrap'>
+                                                    <div className='document_preview_slide'>
+                                                        <img src={id_bluespace} />
+                                                    </div>
+                                                </div>
+                                                <div className='document_preview_slide_wrap'>
+                                                    <div className='document_preview_slide'>
+                                                        <img src={id_bluespace} />
+                                                    </div>
+                                                </div>
+                                            </Slider>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+
 
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 };
